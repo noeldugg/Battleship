b85139cb877e2db975c18df69241cbb3f3bb213e
Noel Duggan <noelduggan21@gmail.com>
2024-07-18 08:04:56 +0000

    Added user_input function that will provide a min and max value

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+    
+    
+def user_input(min_value: int = 1,max_value: int = 10) -> int:
+    """
+    Reads an integer that will provide a min and max value
+     """
+    while True:
+        line = input(prompt)
+        try: 
+            value = int(line)
+            if value < min_value:
+                print(f"The minium value is {min_value}. Try again.")
+            elif value > max_value:
+                print(f"The maxium value is {max_value}. Try again.")
+            else: 
+                return value
+        except ValueError:
+            print("Not a number, try again")
+             
+
+
